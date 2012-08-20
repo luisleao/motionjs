fc3685f1bc83a0d0d18e5e5ee75324084b396412
Luis Leao <luis.leao@gmail.com>
2012-08-20 20:18:26 -0300

    change background loader.

--- js/background.js
+++ js/background.js
@@ -1,4 +1,8 @@
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> change background loader.
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('comm.html', {frame: 'custom', height: 550, width: 800});
 });
