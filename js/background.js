8a30632a978d0f7e8ff1911686f6fbb28218a572
Luis Leao <luis.leao@gmail.com>
2012-08-20 20:25:53 -0300

    changed onLauched function (not experimental)

--- js/background.js
+++ js/background.js
@@ -1,8 +1,3 @@
-
-<<<<<<< HEAD
-=======
-
->>>>>>> change background loader.
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('comm.html', {frame: 'custom', height: 550, width: 800});
 });
